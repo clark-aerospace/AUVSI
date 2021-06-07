4304bf77a1ce95ad0befe27b235798fafd402651
lbenning1989 <lbenning.clark.aerospace@gmail.com>
2021-06-07 07:22:54 -0700

    Change static convertTo methods in class methods

--- auvsi_system/communication/Mission.py
+++ auvsi_system/communication/Mission.py
@@ -5,12 +5,10 @@ transferred to a UAV. The class contains methods to parse mission json files
 compatible messages as well as methods to print out different mission items
 '''
 
-from parseJson import ParseJsonFile
-from mavsdk.mission_raw import MissionItem as RawMissionItem
+from ParseJson import ParseJsonFile
+from mavsdk.mission_raw import MissionItem 
 from mavsdk.geofence import (Point, Polygon) 
 from pygeodesy.ellipsoidalVincenty import LatLon
-
-
 
 class Mission:
     def __init__(self, jsonFile, homePosition):
@@ -25,61 +23,6 @@ class Mission:
         self.searchGrid = self.jsonFile.getSearchGrid()
         # Add the home position to the end of the waypoint list
         self.waypointList.append(self.home)
-               
-    def getMissionWaypoints(self):
-        '''
-        Returns a list of raw mission waypoint items
-        '''
-
-        self.missionWaypoints = convertToWaypointMission(self.waypointList)
-        return self.missionWaypoints
-
-    def getMissionGeofence(self):
-        '''
-        Returns a list of geofence polygons, each containing geofence points and 
-        fence type
-        '''
-
-        self.missionGeofence = convertToBoundaryPointPolygon(self.boundaryPoints)
-        return self.missionGeofence
-
-    def getMissionObstacles(self):
-        '''
-        Returns a list of geofence polygons, each containing a circular obstacle represented
-        as an EXCLUSION zone polygon
-        '''
-
-        self.missionObstacles = convertToObstaclePolygons(self.obstacles)
-        return self.missionObstacles
-
-    def getMissionWaypointArray(self):
-        '''
-        Returns a list of lists version of the mission waypoints, currently used
-        by InformedRRTStar. Currently configured to return 2D array. **Will upgrade
-        to 3D**
-        '''
-
-        waypointArray = []
-        for waypoint in self.waypointList:
-            waypointArray.append([waypoint["longitude"], waypoint["latitude"]])
-
-        return waypointArray
-
-    def getMissionObstacleArray(self):
-
-        obstacleArray = []
-        for obstacle in self.obstacles:
-            obstacleArray.append([obstacle["longitude"], obstacle["latitude"]])
-
-        return obstacleArray
-
-    def getMissionGeofenceArray(self):
-
-        boundaryPointArray = []
-        for boundaryPoint in self.boundaryPoints:
-            boundaryPointArray.append([boundaryPoint["longitude"], boundaryPoint["latitude"]])
-
-        return boundaryPointArray
 
     @staticmethod
     def ConvertFootToMeter(foot):
@@ -125,30 +68,28 @@ class Mission:
                             param1, param2, param3, param4,
                             lat, lon, alt, mission_type)
 
-    @staticmethod
-    def convertToWaypointMission(waypointList):
+    def convertToWaypointMission(self):
         '''
         Accepts a list of waypoint dictionaries and returns a list of raw mission items
         '''
 
         MissionItems = []
-        for seq, waypoint in enumerate(waypointList):
+        for seq, waypoint in enumerate(self.waypointList):
             latitude = int(waypoint["latitude"]*10**7)
             longitude = int(waypoint["longitude"]*10**7)
-            altitude =  ConvertFootToMeter(waypoint["altitude"])
+            altitude =  self.ConvertFootToMeter(waypoint["altitude"])
             if seq == 0:
-                current_waypoint = vtolTakeoffWaypoint(seq, latitude, longitude, altitude)
-            elif seq == len(waypointList) - 1:
-                current_waypoint = vtolLandWaypoint(seq, latitude, longitude, altitude)
+                current_waypoint = self.vtolTakeoffWaypoint(seq, latitude, longitude, altitude)
+            elif seq == len(self.waypointList) - 1:
+                current_waypoint = self.vtolLandWaypoint(seq, latitude, longitude, altitude)
             else:
-                current_waypoint = standardWaypoint(seq, latitude, longitude, altitude)
+                current_waypoint = self.standardWaypoint(seq, latitude, longitude, altitude)
     
             MissionItems.append(current_waypoint)
             
         return MissionItems
     
-    @staticmethod
-    def convertToBoundaryPointPolygon(boundaryPointList):
+    def convertToBoundaryPointPolygon(self):
         '''
         Accepts a list of boundary point dictionaries and returns a list of list 
         mavsdk geofence polygons
@@ -157,7 +98,7 @@ class Mission:
         geofencePointList = []
     
         # Convert boundary point dictionarie into mavsdk geofence points
-        for boundaryPoint in boundaryPointList:
+        for boundaryPoint in self.boundaryPoints:
            geofencePoint = Point(boundaryPoint["latitude"], boundaryPoint["longitude"]) 
            geofencePointList.append(geofencePoint)
         
@@ -166,8 +107,7 @@ class Mission:
     
         return [geofencePolygon]  
     
-    @staticmethod
-    def convertToObstaclePolygons(obstacleList, numVertices = 8):
+    def convertToObstaclePolygons(self, numVertices = 8):
         '''
         Accepts a list of obstacle dictionaries and returns a list of mavsdk compatible 
         obstacle polygons
@@ -176,13 +116,13 @@ class Mission:
         # Initialize list to hold obstacle polygons
         obstaclePolygonList = []
     
-        for obstacle in obstacleList:
+        for obstacle in self.obstacles:
             # Initialize list to hold points in each obstacle polygon
             obstaclePointList = []
             # Capture data from the current obstacle
             latitude = obstacle["latitude"]
             longitude = obstacle["longitude"]
-            radius = ConvertFootToMeter(obstacle["radius"])
+            radius = self.ConvertFootToMeter(obstacle["radius"])
             # Initialize ellipsoidal Vincenty point
             obstaclePoint = LatLon(latitude, longitude)
             # For each vertex point in the polygon that represents the circular obstacle
@@ -200,6 +140,62 @@ class Mission:
             obstaclePolygonList.append(obstaclePolygon)
 
         return obstaclePolygonList
+
+              
+    def getMissionWaypoints(self):
+        '''
+        Returns a list of raw mission waypoint items
+        '''
+
+        self.missionWaypoints = self.convertToWaypointMission()
+        return self.missionWaypoints
+
+    def getMissionGeofence(self):
+        '''
+        Returns a list of geofence polygons, each containing geofence points and 
+        fence type
+        '''
+
+        self.missionGeofence = self.convertToBoundaryPointPolygon()
+        return self.missionGeofence
+
+    def getMissionObstacles(self):
+        '''
+        Returns a list of geofence polygons, each containing a circular obstacle represented
+        as an EXCLUSION zone polygon
+        '''
+
+        self.missionObstacles = self.convertToObstaclePolygons()
+        return self.missionObstacles
+
+    def getMissionWaypointArray(self):
+        '''
+        Returns a list of lists version of the mission waypoints, currently used
+        by InformedRRTStar. Currently configured to return 2D array. **Will upgrade
+        to 3D**
+        '''
+
+        waypointArray = []
+        for waypoint in self.waypointList:
+            waypointArray.append([waypoint["longitude"], waypoint["latitude"]])
+
+        return waypointArray
+
+    def getMissionObstacleArray(self):
+
+        obstacleArray = []
+        for obstacle in self.obstacles:
+            obstacleArray.append([obstacle["longitude"], obstacle["latitude"]])
+
+        return obstacleArray
+
+    def getMissionGeofenceArray(self):
+
+        boundaryPointArray = []
+        for boundaryPoint in self.boundaryPoints:
+            boundaryPointArray.append([boundaryPoint["longitude"], boundaryPoint["latitude"]])
+
+        return boundaryPointArray
 
     def printWaypointList(self):
         print(self.waypointList)
